2c1fd99d6433c3938cb638b5861f8e9e576130e0
Jochem Sluijter <126278247+jsluijter02@users.noreply.github.com>
2024-11-20 10:59:43 +0100

    broken maar pipeline outline verder gebouwd

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,5 +1,21 @@
+from preprocess import Preprocessor
+from eval import Evaluator
+from postprocess import Postprocessor
 class Pipeline:
     def __init__(self):
-        ...
-    def run():
-        ...
+        self.preprocessor = Preprocessor()
+        # self.model = config[model]
+        self.evaluator = Evaluator()
+        self.postprocessor = Postprocessor()
+
+    def run(self):
+        df = self.preprocessor.load_data()
+
+        # X/y?
+        # fit the model
+        self.model.fit(X, y)
+
+        # make a prediction:
+        y_pred = self.model.predict(X, y)
+
+        # 
